ade51bc0241633b984dc193aa3e6e26c6872abec
vipulbhj <2015pcecsvipul119@poornima.org>
2018-12-27 20:06:15 +0530

    First Draft of documentation comment addded

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
-# Warning:- Not up to date 
-
-
 ## Not-Really-A-Framework
-First things first, if you can think of a better name, please open up an issue and rename this thing because I personally hate this name but I am out of thoughts. __*"Index.js" file in the root directory serves as an example usage*.__
+
+### What is it:-
+**NRAF** a lightweight *NODE* framework which provides an alternate to most of the tools out there like **express**,**fastify**, etc(Just kidding).
+
+The intent of this project is mostly for learning purposes, to serve as an example implementation of most of the big guns out there(mentioned above). NRAF is designed keeping in mind that it can be ***intimidating*** for people(especially new developer's) to take a look under the hood and thus NRAF by its design encourages you to look under the hood and may be learn a new thing or two (or open an issue, so that I can learn a thing or two).
+
+The content, implementation and application of this project is immensely inspired by express and you'll also feel the same while working with it.
+
+___
+
+### Applications:- 
+* Small to medium size projects which you might be hacking together on the weekend. NRAF works immensely well for those apps because of
+    * lightweight.
+    * extensibility.
+    * almost flat learning curve(assuming you have done this before).
+* Learning and teaching resource.
+* To be transformed into something bigger by extending.
 
 ## Getting Started:- 
-* npm init
-* npm install nraf --save
-* require('nraf');
+Current no useful documentation is available for the project, but I am actively working on that(might be work to star the project to keep an eye over it).
+
+But in the mean time, If you one of the brave one's. Look into the example directory and open up example.js. You will find comments describing most of the things there are and I have also hacked together a fun example which demonstrates most of the regular bits.
 
 
 *_Spread the word and give this project some love_*
 
-# TL;DR
+___
+
+### TL;DR
 *The motivation for this came from the constant news of issues with npm ecosystem. As this started catching fire, I tried talking a look inside the express git repo and it was very intmedating, thus I started build this to get ideas on how things might work and come up with implementations of the features from popular frameworks. Soon I realized that there is a huge lack of content for creating apps using node without any framwork. Thus I decided to document everything as I go by learning and exploring new concepts and packages which can be used to make node app without any Framework*
 
 
@@ -23,24 +38,8 @@ First things first, if you can think of a better name, please open up an issue a
 * Supports POST requests.
 
 ### Coming Soon:- 
-* Better API to interact with request and response.
-* Additional Features ...
-
-
-## Ooh, btw I built a website using this, which is available [here](https://intense-dusk-47643.herokuapp.com/).
-
-### Why:-
-This project is aimed at learning some internals of designing a functional framework similar to the likes of express, sails, etc build most to the tools and features myself.
-
-### What can I use this for:-
-* As a starterkit for small to medium size personal projects.
-* As a starterkit for playing around with framework design and doing low level things.
-* As learning material for
-    * Node Fundamentals
-    * Promises, Streams, etc.
-    * Some third party modules
-    * Build apps using node, not some framework.
-    * Teaching example or case study.
+* Middleware support (personally I love this feature most about express).
+* Additional Features...
 
 ## Issues, Suggestions and Other things:-
 Pull Requests for anything of the above are appreciated and welcomed immensely.   
--- examples/example.js
+++ examples/example.js
@@ -1,19 +1,53 @@
+/**
+ * import nraf into your application.
+ * If using in an npm project, you will download the latest version by doing 
+ *  - npm install nraf --save
+ * and then replace the "../" with "nraf"
+ */
 const nraf = require('../');
+/**
+ * staticFilePath is exactly what it sounds like, it the absolute path to the directory where your static
+ * resources are placed.
+ */
 const staticFilePath = __dirname + '/public';
-const app = nraf();
+/**
+ * app - It is the actual app object that we will interact with for our application.
+ * It has one parameter which is optional. 
+ * The parameter is the absolute path of static resources.
+ * It has basically two modes:-
+ *  - If paramter is passed, for an undefined route(one which we haven't defined on this app object)
+ *      it will check your public directory and server the file if found or will give 
+ *      "no such file found" message.
+ *  - If no parameter, it simply logs back the url in json format as a response.
+ */
+const app = nraf(staticFilePath);
 
+// PORT on which the server will run.
 const PORT = 5000;
 
-// This route demonstrates construction of get endpoint.
+// This route demonstrates construction of "GET" endpoint.
 app.get('/docs', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(`<h3>Docs Page</h3>`);
+    // send method is used to send text/html data. 
+    res.send(`<h3>Docs Page</h3>`);
+    // end method ends the responses and sends it out to the client.
+    res.end();
+});
+
+app.get('/json-example', (req, res) => {
+    // json method is used to send json/application data back to client. 
+    // Ideal usecase on REST API design.
+    const obj = {
+        name: "Not Really Framework",
+        author: "Vipul Bhardwaj",
+        github: "https://github.com/vipulbhj"
+    };
+    res.json(obj);
     res.end();
 });
 
 // Parameters passed in GET requests are available in the query object on the req object.
 app.get('/yourname', (req, res) => {
-     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.setStatus(200);
     // req.query.name is accessing the name parameter passed by user.
     const htmlResponse = `
         <h3>Your name is ${req.query.name || 'Unknown. Please try playing first'} </h3>
@@ -22,12 +56,11 @@ app.get('/yourname', (req, res) => {
             <input type="submit" value="GO !"/>
         </form>
     `;
-    res.write(htmlResponse);
+    res.send(htmlResponse);
     res.end();
 });
 
 app.get('/login', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
     const htmlResponse = `
         <!-- USERNAME:- abc, password:- 123 --!>
         <h3>Login Form</h3>
@@ -39,24 +72,28 @@ app.get('/login', (req, res) => {
             <input type="submit" value="GO !"/>
         </form>
     `;
-    res.write(htmlResponse);
+    res.send(htmlResponse);
     res.end();
 });
 
+// Demonstartes POST route.
 app.post('/login', (req, res) => {
+    // req.body object holds all the parameters passed in with "POST" request
     const username = req.body.username;
+    console.log(typeof username);
     const password = req.body.password;
     if(username === 'abc' && password === '123') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        // non-json type data could also be directly passed to the end method.
         res.end("Nailed it, You are a <strong>pro SIR</strong>!!!!!!");
     } else {
-        res.writeHead(302, {'Location': '/login?loginError=true'});
+        // redirect takes one parameter which is the location where redirection takes place.
+        res.redirect('/login?loginError=true');
         res.end();
     }
 });
 
 
-
+// Starts the server on PORT(specified above) and callback function which is called when the server has stared.
 app.listen(PORT, () => {
     console.log('PORT:',PORT);
 });
