4b10c9b1b1752f088f30ae7531ee021c6af2a0ca
Vipulbhj <vipulb@geekyants.com>
2021-12-26 18:30:33 +0530

    Cleaned Project, removed everything related to estatic

---
+++ core/examples/CRUD.js
@@ -0,0 +1,40 @@
+const NRAF = require("../index");
+
+const app = NRAF();
+
+const PORT = 3000;
+
+const USERS = [
+  {
+    id: "1",
+    name: "John Doe",
+  },
+  {
+    id: "2",
+    name: "Foo Bar",
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.json(USERS);
+  res.end();
+});
+
+app.get("/user/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const user = USERS.find((user) => {
+    return user.id === userId;
+  });
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.setStatus(401);
+    res.json({
+      message: "Error, no user found",
+    });
+  }
+  res.end();
+});
+
+app.listen(PORT);
